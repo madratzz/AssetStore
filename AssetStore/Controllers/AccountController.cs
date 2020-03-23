039acf307a3948da770ccb8599ad68808c56dfa2
Raza Butt <raxashafique@gmail.com>
2020-03-24 00:17:44 +0500

    Imported Theme

---
+++ AssetStore/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AssetStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ AssetStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AssetStore.MvcApplication" Language="C#" %>
